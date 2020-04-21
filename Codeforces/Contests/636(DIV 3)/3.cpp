4e6a4bd7e164dee8bbe88952dd5d0f09cfbfe6e2
SparkXV <deepanshuvijay123@gmail.com>
2020-04-21 22:47:35 +0530

    Contest

--- Codeforces/Contests/636(DIV 3)/3.cpp
+++ Codeforces/Contests/636(DIV 3)/3.cpp
@@ -6,79 +6,87 @@
 #include<set>
 #include<vector>
 #define MOD 1000000007
-#define loop(a,b) for(ll i=a;i<b;i++)
+#define loop(a,b) for(int i=a;i<b;i++)
 #define fast_ip ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
+
 
 int main() {
 	// your code goes here
 	
 	ll t;
 	cin>>t;
-    /*
-	for (ll i=0; i<=100001; i++) 
-    {
-          prime[i]=true;
-    }
-    sieve();
-    */
 	while(t--)
 	{
 	    int n;
-        cin>>n; 
-        int a[n];
-        loop(0,n)
+	    cin>>n;
+	    int a[n];
+	   for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    if(n==1)
         {
-            cin>>a[i];
+            cout<<a[n-1]<<"\n";
         }
-        int dp[n];
-        dp[n-1]=0;
-        int le=1;
-        int p;
-        for(int i=n-2;i>=0;i--)
+        else
         {
+           int p;
+           int dp[n]; 
+           dp[n-1]=1;
+           ll sum[n];
+           sum[n-1]=a[n-1];
+           for(int i=n-2;i>=0;i--)
+           {
                p=INT_MIN;
+               int index;
+               int prev;
                for(int j=i+1;j<n;j++)
                {
-                   if((a[j]>=0 && a[i]<=0) || (a[j]<=0 && a[i]>=0))
+                   if((a[j]>=0 && a[i]<=0) ||(a[j]<=0 && a[i]>=0))
                    {
-                        p=max(p,dp[j]);
-                   }     
-               }
+                       if(p<dp[j])
+                       {
+                          p=max(p,dp[j]);
+                          prev=a[j];
+                          index=j;
+                        } 
+                        else if(p==dp[j])
+                        {
+                            if(a[j]>prev)
+                            {
+                                index=j;
+                            }
+                        }
+                    }
+                 }
                if(p==INT_MIN)
                {
-                   dp[i]=0;
+                   dp[i]=1;
+                   sum[i]=a[i];
                }
                else
-                  dp[i]=1+p;
-        }
-        p=INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            p=max(p,dp[i]);
-        }
-        int sum;
-        int res;
-        int dp1[n];
-        dp1[n-1]=0;
-        for(int i=n-2;i>=0;i--)
-        {
-               sum=INT_MIN;
-               for(int j=i+1;j<n;j++)
                {
-                   if((a[j]>=0 && a[i]<=0) || (a[j]<=0 && a[i]>=0))
-                   {
-                        sum=max(sum,dp1[j]);
-                   }     
+                   dp[i]=1+p;
+                   sum[i]=a[i]+sum[index];
                }
-               if(p==INT_MIN)
-               {
-                   dp1[i]=0;
-               }
-               else
-                  dp1[i]=1+sum;
-               if(dp1[i])
+            }
+            p=INT_MIN;
+            for(int i=0;i<n;i++)
+            {
+                p=max(p,dp[i]);
+            }
+            ll res=LLONG_MIN;
+            for(int i=0;i<n;i++)
+            {
+                if(p==dp[i])
+                {
+                    res=max(res,sum[i]);
+                }
+            }
+            cout<<res<<"\n";
         }
-	}
+    }
 	return 0;
 }
+
